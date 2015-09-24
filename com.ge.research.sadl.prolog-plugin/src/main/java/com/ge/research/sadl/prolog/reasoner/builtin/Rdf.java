de1f4e48dcb030c268e7a27f4177560681566ea7
Andrew Crapo <crapo@research.ge.com>
2015-09-24 11:30:38 -0400

    Initial checkin of Prolog reasoner wrapper for sadl

---
+++ com.ge.research.sadl.prolog-plugin/src/main/java/com/ge/research/sadl/prolog/reasoner/builtin/Rdf.java
@@ -0,0 +1,68 @@
+/************************************************************************
+ * Copyright \u00a9 2007, 2008 - General Electric Company, All Rights Reserved
+ * 
+ * Project: SADL
+ * 
+ * Description: The Semantic Application Design Language (SADL) is a 
+ * language for building semantic models and expressing rules that 
+ * capture additional domain knowledge. The SADL-IDE (integrated 
+ * development environment) is a set of Eclipse plug-ins that 
+ * support the editing and testing of semantic models using the 
+ * SADL language.
+ * 
+ * This software is distributed "AS-IS" without ANY WARRANTIES 
+ * and licensed under the Eclipse Public License - v 1.0 
+ * which is available at http://www.eclipse.org/org/documents/epl-v10.php
+ *
+ ***********************************************************************/
+
+/***********************************************************************
+ * $Author: rpalla $ 
+ * $Revision: 1.1 $ Last modified on   $Date: 2014/11/10 16:40:36 $
+ ***********************************************************************/
+
+package com.ge.research.sadl.prolog.reasoner.builtin;
+
+import com.hp.hpl.jena.reasoner.rulesys.*;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.graph.*;
+
+/**
+
+ */
+public class Rdf extends com.hp.hpl.jena.reasoner.rulesys.builtins.BaseBuiltin {
+
+	private int argLength = 0;
+
+    /**
+     * Return the expected number of arguments for this functor or 0 if the number is flexible.
+     */
+    public int getArgLength() {
+        return argLength;
+    }
+    
+    /**
+     * Return a name for this builtin, normally this will be the name of the 
+     * functor that will be used to invoke it.
+     */
+    @Override
+    public String getName() {
+        return "rdf";
+    }
+    /**
+     * This method is invoked when the builtin is called in a rule body.
+     * @param args the array of argument values for the builtin, this is an array 
+     * of Nodes, some of which may be Node_RuleVariables.
+     * @param length the length of the argument list, may be less than the length of the args array
+     * for some rule engines
+     * @param context an execution context giving access to other relevant data
+     * @return return true if the buildin predicate is deemed to have succeeded in
+     * the current environment
+     */
+    public boolean bodyCall(Node[] args, int length, RuleContext context) {
+        checkArgs(length, context);
+        return true;
+    }
+    
+   
+}
