79b1b0111fe2217df89642d76e37644ac501ccbd
Nicholas Bachman <nicholas.bachman@ge.com>
2016-06-20 09:15:42 -0400

    Fixed merge conflict.

--- sadl3/com.ge.research.sadl.parent/com.ge.research.sadl/src/com/ge/research/sadl/utils/ResourceManager.java
+++ sadl3/com.ge.research.sadl.parent/com.ge.research.sadl/src/com/ge/research/sadl/utils/ResourceManager.java
@@ -99,26 +99,7 @@ public class ResourceManager {
 		return prjuri;
     }
     
-<<<<<<< HEAD
     public static URI findProjectUriByTrimming(URI uri){
-    	File file = new File(uri.toFileString());
-    	if(file != null){
-    		if(file.isDirectory()){
-    			for(String child : file.list()){
-    				if(child.endsWith(".project")){
-    					return uri;
-    				}
-    			}
-    			//Didn't find a project file in this directory, check parent
-    			if(file.getParentFile() != null){
-    				return findProjectUriByTrimming(uri.trimSegments(1));
-    			}
-    		}
-    		if(file.isFile() && file.getParentFile() != null){
-    			return findProjectUriByTrimming(uri.trimSegments(1));
-    		}
-=======
-    private static URI findProjectUriByTrimming(URI uri){
     	String fstr = uri.toFileString();
     	if (fstr != null) {
 	    	File file = new File(uri.toFileString());
@@ -138,7 +119,7 @@ public class ResourceManager {
 	    			return findProjectUriByTrimming(uri.trimSegments(1));
 	    		}
 	    	}
->>>>>>> master
+
     	}
     	
     	return null;
