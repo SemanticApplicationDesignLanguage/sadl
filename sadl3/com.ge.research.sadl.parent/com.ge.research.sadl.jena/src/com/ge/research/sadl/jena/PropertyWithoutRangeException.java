88f2f07b7a432e5935aeace52eb74ff2cf617e30
Andy Crapo <crapo@ge.com>
2017-05-24 09:12:17 -0400

    an exception with no message can create an error marker with no text, which is a bit confusing.

--- sadl3/com.ge.research.sadl.parent/com.ge.research.sadl.jena/src/com/ge/research/sadl/jena/PropertyWithoutRangeException.java
+++ sadl3/com.ge.research.sadl.parent/com.ge.research.sadl.jena/src/com/ge/research/sadl/jena/PropertyWithoutRangeException.java
@@ -10,6 +10,6 @@ public class PropertyWithoutRangeException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public PropertyWithoutRangeException(){
-		super();
+		super("Property does not have a range");
 	}
 }
