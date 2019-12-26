2c53a1c51738d869fff54605dac8ce777e734b40
awcrapo <crapo@research.ge.com>
2019-12-26 13:35:55 -0500

    organized imports

--- sadl3/com.ge.research.sadl.parent/com.ge.research.sadl.jena/src/com/ge/research/sadl/jena/IntermediateFormTranslator.java
+++ sadl3/com.ge.research.sadl.parent/com.ge.research.sadl.jena/src/com/ge/research/sadl/jena/IntermediateFormTranslator.java
@@ -19,7 +19,6 @@
  package com.ge.research.sadl.jena;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
