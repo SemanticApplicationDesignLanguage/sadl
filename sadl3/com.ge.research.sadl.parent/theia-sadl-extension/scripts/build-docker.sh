7b17349f335ef274d70d26b9d9e16f433d3e4642
Akos Kitta <kittaakos@typefox.io>
2020-04-30 14:48:18 +0200

    Moved the Docker image creation to a script.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

---
+++ sadl3/com.ge.research.sadl.parent/theia-sadl-extension/scripts/build-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd "$(dirname "$0")" \
+&& cd ../browser-app \
+&& cp -rf ../yarn.lock . \
+&& NEXT_VERSION=`npm view sadl-extension@next version` \
+&& mv -f ./package.json ./package.json.original \
+&& cat ./package.json.original | jq .version=\"$NEXT_VERSION\" > package.json \
+&& docker build --build-arg "GITHUB_TOKEN=$GITHUB_TOKEN" --no-cache . -t theiaide/sadl:$NEXT_VERSION \
+&& docker tag theiaide/sadl:$NEXT_VERSION theiaide/sadl:next \
+&& rm ./package.json \
+&& mv -f ./package.json.original ./package.json
