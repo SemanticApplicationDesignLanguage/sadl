e00c8b3222d9278b5339393c397dc4c609404a79
Akos Kitta <kittaakos@typefox.io>
2020-05-04 14:14:57 +0200

    Pinned `moment` version.
    Fix for eclipse-theia/theia#7726
    
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- sadl3/com.ge.research.sadl.parent/theia-sadl-extension/scripts/build-docker.sh
+++ sadl3/com.ge.research.sadl.parent/theia-sadl-extension/scripts/build-docker.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")" \
 && cp -rf ../yarn.lock . \
 && NEXT_VERSION=`npm view sadl-extension@next version` \
 && mv -f ./package.json ./package.json.original \
-&& cat ./package.json.original | jq .version=\"$NEXT_VERSION\" > package.json \
+&& cat ./package.json.original | jq .version=\"$NEXT_VERSION\" | jq .dependencies.\"sadl-extension\"=\"$NEXT_VERSION\" > package.json \
 && docker build --build-arg "GITHUB_TOKEN=$GITHUB_TOKEN" --no-cache . -t theiaide/sadl:$NEXT_VERSION \
 && docker tag theiaide/sadl:$NEXT_VERSION theiaide/sadl:next \
 && rm ./package.json \
