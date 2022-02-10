3e2bc72e33d90d8d2fe541727c85b019dd813473
Andrew Crapo <awcrapo@gmail.com>
2022-02-10 12:34:55 -0500

    fix to GH issue 876 (#880)
    * fix to GH issue 876
    
    * documented strategy

--- sadl3/com.ge.research.sadl.parent/com.ge.research.sadl.reasoner/com.ge.research.sadl.reasoner-impl/src/main/java/com/ge/research/sadl/model/persistence/SadlJenaTDBGetterPutter.java
+++ sadl3/com.ge.research.sadl.parent/com.ge.research.sadl.reasoner/com.ge.research.sadl.reasoner-impl/src/main/java/com/ge/research/sadl/model/persistence/SadlJenaTDBGetterPutter.java
@@ -17,6 +17,14 @@ import com.ge.research.sadl.reasoner.IConfigurationManager;
 import com.ge.research.sadl.reasoner.IConfigurationManagerForEditing;
 import com.ge.research.sadl.reasoner.TranslationException;
 
+/**
+ * Strategy: all read calls to SadlJenaTDBGetter check and if needed open READ transaction. 
+ * Putter checks for transaction and if true ends, opens WRITE transaction, does save, ends, 
+ * reopens READ transaction.
+ * 
+ * @author Natural Semantics, LLC
+ *
+ */
 public class SadlJenaTDBGetterPutter extends SadlJenaTDBGetter implements ISadlModelGetterPutter {
 
 	public SadlJenaTDBGetterPutter(IConfigurationManager mgr, String fmt) throws IOException {
@@ -24,9 +32,12 @@ public class SadlJenaTDBGetterPutter extends SadlJenaTDBGetter implements ISadlM
 	}
 
 	@Override
-	public boolean saveModel(Model m, String modelNamespace, String publicUri, String OwlFileName, String format)
+	public boolean saveModel(Model m, String modelNamespace, String publicUri, String owlFileName, String format)
 			throws TranslationException {
 		try {
+			if (ds.isInTransaction()) {
+				ds.end();
+			}
 	    	ds.begin(ReadWrite.WRITE);
 			Model modelToRemove = ds.getNamedModel(publicUri);
 			if (modelToRemove != null) {
@@ -49,26 +60,62 @@ public class SadlJenaTDBGetterPutter extends SadlJenaTDBGetter implements ISadlM
 		}
 		finally {
 			ds.end();
+			ds.begin(ReadWrite.READ);
 		}
-		File mfile = new File(OwlFileName);
-		if (mfile.exists()) {
-			// the OWL file exists and we just saved the OWL model to TDB, so delete the OWL file
-			if (!mfile.delete()) {
-				logger.error("Failed to delete OWL file '" + OwlFileName + "' after saving it to TDB");
+		if (owlFileName.endsWith(".tdb")) {
+			File existingOwlFile = findExistingOwlFilename(owlFileName);
+			if (existingOwlFile !=  null) {
+				// the OWL file exists and we just saved the OWL model to TDB, so delete the OWL file
+				if (!existingOwlFile.delete()) {
+					try {
+						logger.error("Failed to delete OWL file '" + existingOwlFile.getCanonicalPath() + "' after saving it to TDB");
+					} catch (IOException e) {
+						// not worth doing anything?
+					}
+				}
 			}
 		}
 		return true;
 	}
 	
+	/**
+	 * Method to find a left-over OWL file from non-TDB if one exists
+	 * @param owlFileName
+	 * @return
+	 */
+	private File findExistingOwlFilename(String owlFileName) {
+		int idx = owlFileName.lastIndexOf(".");
+		if (idx > 0) {
+			String baseFN = owlFileName.substring(0, idx + 1);
+			List<String> extensions = SadlPersistenceFormat.getOwlFileExtensions();
+			for (String ext : extensions) {
+				File f = new File(baseFN + ext);
+				if (f.exists()) {
+					return f;
+				}
+			}
+		}
+		return null;
+	}
+
 	@Override
 	public boolean removeModel(String uri) throws IOException {
 		if (getConfigMgr() instanceof IConfigurationManagerForEditing) {
-			if (ds.containsNamedModel(uri)) {
-				ds.begin(ReadWrite.WRITE);
-				ds.removeNamedModel(uri);
-				ds.commit();
-				TDB.sync(ds);
-				return true;
+			try {
+				if (ds.containsNamedModel(uri)) {
+					if (ds.isInTransaction()) {
+						ds.end();
+					}
+					ds.begin(ReadWrite.WRITE);
+					ds.removeNamedModel(uri);
+					ds.commit();
+					TDB.sync(ds);
+					return true;
+				}
+			}
+			finally {
+				ds.end();
+				ds.begin(ReadWrite.READ);
 			}
 		} else {
 			throw new IOException("Can't remove models with the current ConfigurationManager class: " + getConfigMgr().getClass().getCanonicalName());
@@ -82,32 +129,37 @@ public class SadlJenaTDBGetterPutter extends SadlJenaTDBGetter implements ISadlM
      * @throws ConfigurationException
      */
 	@Override
-    public int clean() throws ConfigurationException {
-    	if (ds != null) {
-    		int cnt = 0;
+	public int clean() throws ConfigurationException {
+		if (ds != null) {
+			int cnt = 0;
 			List<String> modelsToRemove = new ArrayList<String>();
-    		Iterator<String> namedModelsItr = ds.listNames();
-    		while (namedModelsItr != null && namedModelsItr.hasNext()) {
-    			String modelname = namedModelsItr.next();
-    			modelsToRemove.add(modelname);
-    		}
-    		for (int i = 0; i < modelsToRemove.size(); i++) {
-    			try {
-    				ds.begin(ReadWrite.WRITE);
-   					ds.removeNamedModel(modelsToRemove.get(i));
-   					cnt++;
-    			}
-    			catch (Throwable t) {
-    				throw new ConfigurationException("Unexpected exception: " + t.getMessage());
-    			}
-    			finally {
-    				ds.commit();
-    				TDB.sync(ds);
-   			}
-    		}
-    		return cnt;
-    	}
-    	return 0;
-    }
+			Iterator<String> namedModelsItr = ds.listNames();
+			while (namedModelsItr != null && namedModelsItr.hasNext()) {
+				String modelname = namedModelsItr.next();
+				modelsToRemove.add(modelname);
+			}
+			for (int i = 0; i < modelsToRemove.size(); i++) {
+				try {
+					if (ds.isInTransaction()) {
+						ds.end();
+					}
+					ds.begin(ReadWrite.WRITE);
+					ds.removeNamedModel(modelsToRemove.get(i));
+					cnt++;
+				}
+				catch (Throwable t) {
+					throw new ConfigurationException("Unexpected exception: " + t.getMessage());
+				}
+				finally {
+					ds.commit();
+					TDB.sync(ds);
+					ds.end();
+					ds.begin(ReadWrite.READ);
+				}
+			}
+			return cnt;
+		}
+		return 0;
+	}
     
 }
